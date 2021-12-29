fa16e1b465582cb24d1f537f80464ab07a5a1e0d
Serhat Ataman <Serhat.Ataman@unit4.com>
2021-12-29 09:00:40 +0100

    refinement of course names

--- PythonCourse/Chapter14-Modules/modules.py
+++ PythonCourse/Chapter14-Modules/modules.py
@@ -2,9 +2,15 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 import sys
+import os
+
 
 def main():
     v = sys.version_info
     print('Python version {}.{}.{}'.format(*v))
+    print('Platform: ' + str(sys.platform))
+    print('Path: ' + str(os.path))
 
-if __name__ == '__main__': main()
+
+if __name__ == '__main__':
+    main()
