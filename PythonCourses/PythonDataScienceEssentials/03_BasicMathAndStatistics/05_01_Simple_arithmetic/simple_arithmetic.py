1673c40ae95985d25741cb365c1392a58b79d9ff
Serhat Ataman <serhatataman13@hotmail.com>
2022-02-22 09:01:48 +0100

    started linear algebra

--- PythonCourses/PythonDataScienceEssentials/03_BasicMathAndStatistics/05_01_Simple_arithmetic/simple_arithmetic.py
+++ PythonCourses/PythonDataScienceEssentials/03_BasicMathAndStatistics/05_01_Simple_arithmetic/simple_arithmetic.py
@@ -14,13 +14,27 @@ print("Array b: " + str(b))
 # Creating arrays via assignment
 # create six random signed numbers
 np.random.seed(25)
-# we want 6 values
+# we want 6 random values
 c = 36 * np.random.randn(6)
 print("Array c: " + str(c))
 
 # get numbers between 1 and 36 as an array
 d = np.arange(1, 36)
 print("Array d: " + str(d))
+
+# Perform arithmetic on arrays
+# multiply array a by 10
+print("Each element of array 'a' is multiplied by 10: " + str(a*10))
+
+# add elements of array a to c
+print("Each element of array 'c' is added by elements of a: " + str(c + a))
+# Subtract elements of array a to c
+print("Each element of array 'c' is subtracted by elements of a: " + str(c - a))
+# Multiply elements of array a to c
+print("Each element of array 'c' is multiplied by elements of a: " + str(c * a))
+# Divide elements of array a to c
+print("Each element of array 'c' is divided by elements of a: " + str(c / a))
+
 
 
 
