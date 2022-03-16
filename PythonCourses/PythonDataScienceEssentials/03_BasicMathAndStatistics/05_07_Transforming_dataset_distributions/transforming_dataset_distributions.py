3a38cefd0a1c505957412a8090eedbf3c08de436
Serhat Ataman <serhatataman13@hotmail.com>
2022-03-16 12:19:07 +0100

    transforming dataset distributions is started

---
+++ PythonCourses/PythonDataScienceEssentials/03_BasicMathAndStatistics/05_07_Transforming_dataset_distributions/transforming_dataset_distributions.py
@@ -0,0 +1,12 @@
+import matplotlib_inline.config
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sb
+from pylab import rcParams
+
+import scipy
+from scipy.stats.stats import pearsonr, spearmanr
+from scipy.stats import chi2_contingency
+
