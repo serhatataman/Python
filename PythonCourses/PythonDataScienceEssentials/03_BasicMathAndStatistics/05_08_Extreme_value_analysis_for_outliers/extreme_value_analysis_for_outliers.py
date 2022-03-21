6ceeecbecc9045eff457b1179484838dbc8c698d
Serhat Ataman <serhatataman13@hotmail.com>
2022-03-21 17:05:31 +0100

    extreme value analysis for outliers course is started

--- PythonCourses/PythonDataScienceEssentials/03_BasicMathAndStatistics/05_08_Extreme_value_analysis_for_outliers/extreme_value_analysis_for_outliers.py
+++ PythonCourses/PythonDataScienceEssentials/03_BasicMathAndStatistics/05_08_Extreme_value_analysis_for_outliers/extreme_value_analysis_for_outliers.py
@@ -4,19 +4,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pylab import rcParams
 
-rcParams['figure.figsize'] = [8, 4]
-plt.style.use('seaborn-whitegrid')
+rcParams['figure.figsize'] = [5, 4]
 
 # EXTREME VALUE ANALYSIS FOR OUTLIERS
 
+# Extreme value analysis using uni-variate methods
+
+address = "../../Data/iris.data.csv"
+
+df = pd.read_csv(filepath_or_buffer=address, header=None, sep=',')
+df.columns = ["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Species"]
+
+# Use data slicing and select all the rows but only get first 4 columns. Then get values only.
+X = df.iloc[:, 0:4].values
+
+# Y is going to be our target
+y = df.iloc[:, 4].values
+
+# Print first 5 records of the dataframe
+print("\nFirst five records of dataframe: \n" + str(df[:5]))
+
+# IDENTIFYING OUTLIERS FROM TUKEY BOXPLOTS. (yes, the name is Tukey)
+
+# Create boxplot automatically and return a dictionary
+df.boxplot(return_type='dict')
+plt.plot()
+plt.show(block=True)
+
+# Use filtering and comparing operations to isolate outliers (that are found in Sepal_Width)
+# from the rest of the dataframe
+Sepal_Width = X[:, 1]  # isolate Sepal_Width column
+iris_outliers = (Sepal_Width > 4)  # isolate these records where Sepal_Width is greater than 4 so we can understand what's really happening
 
 
-address = "../../Data/mtcars.csv"
-
-cars = pd.read_csv(address)
-cars.columns = ['car_names', 'mpg', 'cyl', 'disp', 'hp', 'drat', 'wt', 'qsec', 'vs', 'am', 'gear', 'carb']
-
-print("\nCars dataset: \n" + str(cars.head()))
-
-# sb.pairplot(cars)  # This prints out all the tables
-# plt.show(block=True)
