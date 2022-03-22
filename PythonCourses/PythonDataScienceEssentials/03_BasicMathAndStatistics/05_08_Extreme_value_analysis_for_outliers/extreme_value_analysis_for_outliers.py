49054806426dda4a4806e5ef76e2f86d1a6ce4ea
Serhat Ataman <serhatataman13@hotmail.com>
2022-03-22 10:00:51 +0100

    extreme value analysis for outliers finished

--- PythonCourses/PythonDataScienceEssentials/03_BasicMathAndStatistics/05_08_Extreme_value_analysis_for_outliers/extreme_value_analysis_for_outliers.py
+++ PythonCourses/PythonDataScienceEssentials/03_BasicMathAndStatistics/05_08_Extreme_value_analysis_for_outliers/extreme_value_analysis_for_outliers.py
@@ -6,6 +6,8 @@ from pylab import rcParams
 
 rcParams['figure.figsize'] = [5, 4]
 
+# IQR : Inter-quartile Range
+
 # EXTREME VALUE ANALYSIS FOR OUTLIERS
 
 # Extreme value analysis using uni-variate methods
@@ -34,6 +36,26 @@ plt.show(block=True)
 # Use filtering and comparing operations to isolate outliers (that are found in Sepal_Width)
 # from the rest of the dataframe
 Sepal_Width = X[:, 1]  # isolate Sepal_Width column
-iris_outliers = (Sepal_Width > 4)  # isolate these records where Sepal_Width is greater than 4 so we can understand what's really happening
+# isolate these records where Sepal_Width is greater than 4, so we can get upper level outliers
+iris_outliers = (Sepal_Width > 4)
 
+# We have found outliers
+print("\nIris above outliers:\n " + str(df[iris_outliers]))
+
+# Let's also isolate values that are below 2 to find bottom outliers
+Sepal_Width = X[:, 1]  # isolate Sepal_Width column
+# isolate these records where Sepal_Width is lower than 2.05, so we can get lower level outliers
+iris_outliers = (Sepal_Width < 2.05)
+
+print("\nIris below outliers:\n " + str(df[iris_outliers]))
+
+
+# APPLYING TUKEY OUTLIER LABELING to detect outliers
+# pd.options.display.float_format = '{:.1f}'.format()
+X_df = pd.DataFrame(X)
+print("\nDescription of Tukey outlier labeling:\n" + str(X_df.describe()))
+
+# To find Interquartile range we need to do IQR = (1.5 x (%75 - %25)).
+# Then, %25 - IQR = lowest point of the data. The data lower than this is an outlier
+# After that, %75 + IQR = highest point of the data. The data higher than this is an outlier
 
