6002c113f7c9d159be5c2c5463d198571533286d
Serhat Ataman <serhatataman13@hotmail.com>
2022-04-13 16:31:00 +0300

    statisctical charts

---
+++ PythonCourses/PythonDataScienceEssentials/05_AnalyticsWithPlotly/07_02_Create_statistical_charts/create_statistical_charts.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import cufflinks as cf
+import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly.tools as tls
+
+
+# todo: signup for plotly and get the api key
+
